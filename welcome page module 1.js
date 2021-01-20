f0a671743b699657d42464353ced450c1bd22a91
Cynthia <cynos1@morgan.edu>
2021-01-19 20:17:07 -0500

    update

--- welcome page module 1.js
+++ welcome page module 1.js
@@ -2,15 +2,17 @@
 $(document).ready( function() {
   
   var text = "Module 1 : Motivating and Preparing.";
+
   var index = 0;
   var chars = text.length - 1;
+
   var speed = 100;
   var div = "typing";
 
   function typing() {
   
     document.getElementById(div).innerHTML += text[index];
-
+   
     if(index < chars){
       setTimeout(function(){
         index++;
@@ -21,7 +23,7 @@ $(document).ready( function() {
     setTimeout(function(){location.href="new activity1.html"
     }, 4200);
   }
-
+   
   // When the browser is load or reaload
   $(window).load(function() {
     $('#overlay').fadeOut();
