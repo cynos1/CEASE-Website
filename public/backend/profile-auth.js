35a5ca72ff64647a8565ef00ff5ca3b72988faa7
isaboll1 <isaboll12@gmail.com>
2021-02-02 22:29:58 -0500

    added interim user identification. it works, but is suboptimal

--- public/backend/profile-auth.js
+++ public/backend/profile-auth.js
@@ -1,9 +1,63 @@
 const signupForm = document.querySelector("#CEASE-signup-form");
+const users = database.collection("users");
 
-const userEmailPhone = document.getElementById("email/phone-textbox").value;
-const userName = document.getElementById("username-textbox").value;
-const nonconfirmedPwd = document.getElementById("password-1-textbox").value;
-const confirmedPwd = document.getElementById("password-2-textbox").value;
+function authorizeAccount(){
+    var UID = sessionStorage.getItem("UID");
+    
+    var usernameUsed = false;
 
+    const userEmailPhone = document.getElementById("email/phone-textbox").value;
+    const userName = document.getElementById("username-textbox").value;
+    const nonconfirmedPwd = document.getElementById("password-1-textbox").value;
+    const confirmedPwd = document.getElementById("password-2-textbox").value;
 
+    
+    //all of the hidden elements to show
+    const emailWarningDiv = document.getElementById('used-email-password');
+    const usernameWarningDiv = document.getElementById('used-username');
+    const passwordWarningDiv = document.getElementById('unmatching-passwords');
+    const emptyError = document.getElementById('empty-error');
 
+    var return_value = false;
+
+    users.where("user-name", "==", userName).get().then(
+        (snapshot)=>{
+            if (snapshot.docs.length){
+                usernameUsed = true;
+            }
+        }
+    );
+
+    if (userEmailPhone.length == 0 ||userName.length == 0 || nonconfirmedPwd == 0 || confirmedPwd == 0){
+        emptyError.style.display = "block";
+        return false;
+    }
+
+    if (nonconfirmedPwd != confirmedPwd){
+        passwordWarningDiv.style.display = "block";
+        return false;
+    }
+    if (usernameUsed){
+        usernameWarningDiv.style.display = "block";
+        return false;
+    }
+
+    auth.createUserWithEmailAndPassword(userEmailPhone, confirmedPwd).then(
+        (userCredentials)=>{
+            var user = userCredentials.user;
+            user.updateProfile({displayName: userName});
+            users.doc(UID).set({
+                'user-name': userName,
+                'registered-code': UID
+            }).then(
+                ()=>{console.log("Added User.")}
+            );
+            sessionStorage.setItem("User", user);
+            location.href='modulesurvey.html';
+        }
+    ).catch((error)=>{
+        if (error.code == 'auth/email-already-in-use'){
+            emailWarningDiv.style.display = "block";
+        }
+    });
+}
--- public/backend/uid-storage.js
+++ public/backend/uid-storage.js
@@ -1,12 +1,14 @@
 
-const uidCacheRef = database.collection("Users").doc("account-id");
+const uidCacheRef = database.collection("codes").doc("codelist");
+const users = database.collection("users");
 const incorrectpwd = document.getElementById("incorrectpwd");
-var codes = []
+var used = [];
+var codes = [];
 
 uidCacheRef.get().then(function(doc) {
     if (doc.exists) {
         var cache = doc.data();
-        codes = cache['user-id'];
+        codes = cache['codes'];
     } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
@@ -16,10 +18,18 @@ uidCacheRef.get().then(function(doc) {
 
 function verifyUID(){
     console.log(codes);
+    var valid = true;
     var userCode = document.getElementById("myInput").value;
     var storage = window.sessionStorage;
     incorrectpwd.style.display = "none";
-    if (codes.indexOf(userCode) != -1){
+    users.where("registered-code","==", userCode)
+                .get().then(
+                    (snapshot)=>{
+                        if (snapshot.docs.length){
+                            valid = false;
+                        }
+                    });
+    if ((codes.indexOf(userCode) != -1) && valid){
         sessionStorage.setItem("UID", userCode);
         console.log('Valid Code');
         return true;
